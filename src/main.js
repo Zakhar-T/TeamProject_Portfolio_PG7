b718a106a7b310bf2afa2d9e92a273e8f628fe0c
Zakhar-T <zakhar.null@gmail.com>
2025-04-02 23:05:10 +0300

    edit file structure & add basic dependancies

---
+++ src/main.js
@@ -0,0 +1,11 @@
+//Imported JavaScript for sections
+
+import "./js/header";
+import "./js/hero";
+import "./js/about-me";
+import "./js/benefits";
+import "./js/projects";
+import "./js/faq";
+import "./js/covers";
+import "./js/reviwes";
+import "./js/work-together";
