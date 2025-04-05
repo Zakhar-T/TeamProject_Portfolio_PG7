2ad0fe17d85b9a99d5633d664f45fea70a409f92
Oleksiy <Unialex@ukr.net>
2025-04-05 19:59:32 +0300

    some header polish

--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,10 @@
-let isUserActive = 0;
+import logoJpg from '/img/header/logo.jpg';
+import uniPng from '/img/header/uni.png';
 const menuBtnOpn = document.getElementById('hdr-openmenu');
 menuBtnOpn.addEventListener('click', onMenuOpenClick);
 
 function onMenuOpenClick(evt) {
-  logo.removeEventListener('mouseover', playAudio);
-  logo.addEventListener('mouseover', playAudio);
+  setNyan();
 
   let menu;
   if (evt.target.tagName === 'SPAN') {
@@ -23,7 +23,6 @@ function onMenuCloseClick(evt) {
   }
 }
 // #region Nyan
-
 const logo = document.querySelector('.hdr-nav-logo');
 let nyanStopDelay;
 let nyanStarted = false;
@@ -31,12 +30,13 @@ let nyanStarted = false;
 function playAudio() {
   logo.classList.add('js-logo');
   let audio = document.getElementById('nyanAudio');
-  audio.play();
   if (!nyanStarted) {
     logo.addEventListener('mouseout', pauseAudio);
     logo.addEventListener('animationend', goNyan);
     nyanStarted = true;
+    audio.currentTime = 1;
   }
+  audio.play();
   clearInterval(nyanStopDelay);
 }
 
@@ -47,7 +47,7 @@ function pauseAudio() {
     audio.currentTime = 0;
     logo.classList.remove('js-logo');
     logo.firstElementChild.style.animation = '';
-    logo.firstElementChild.setAttribute('src', '/img/header/logo.jpg');
+    logo.firstElementChild.setAttribute('src', logoJpg);
     logo.removeEventListener('mouseover', playAudio);
     logo.removeEventListener('mouseout', pauseAudio);
     logo.removeEventListener('animationend', goNyan);
@@ -55,7 +55,18 @@ function pauseAudio() {
   }, 1000);
 }
 function goNyan() {
-  logo.firstElementChild.setAttribute('src', '/img/header/uni.png');
   logo.firstElementChild.style.animation = 'nyan 400ms infinite linear';
+  logo.firstElementChild.setAttribute('src', uniPng);
+}
+
+function setNyan() {
+  const isTouchDevice = () =>
+    'ontouchstart' in window ||
+    navigator.maxTouchPoints > 0 ||
+    document.defaultView.innerWidth < 1440;
+  if (!isTouchDevice()) {
+    logo.removeEventListener('mouseover', playAudio);
+    logo.addEventListener('mouseover', playAudio);
+  }
 }
 // #endregion Nyan
