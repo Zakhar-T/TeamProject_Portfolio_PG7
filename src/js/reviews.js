498c21222e1a096e503fcaaf51af009c5910ee75
GordonVL <ladaniv123@gmail.com>
2025-04-05 21:41:44 +0200

    finish section reviews

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,3 +1,7 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation} from 'swiper/modules';
+
 const rewiewers_list = [
   {
     name: 'Natalie',
@@ -29,18 +33,59 @@ const rewiewers_list = [
   },
 ];
 
-const rewiews = document.querySelector('.rewiews-list');
+const rewiews = document.querySelector('.reviews-list');
 const fragment = document.createDocumentFragment();
 
-rewiewers_list.forEach(({name, alt, img, rewiew}) => {
-    const rewiewer_item = document.createElement('li');
-    rewiewer_item.classList.add('rewiews-item');
-    rewiewer_item.innerHTML = `
-                <img src="${img}" alt="${alt}" class="rewiews-img">
-                <h3 class="rewiews-name">${name}</h3>
-                <p class="rewiews-text-content">${rewiew}</p>
+rewiewers_list.forEach(({ name, alt, img, rewiew }) => {
+  const rewiewer_item = document.createElement('li');
+  rewiewer_item.classList.add('reviews-item');
+  rewiewer_item.classList.add('swiper-slide');
+  rewiewer_item.innerHTML = `
+    <img src="${img}" alt="${alt}" class="reviews-img">
+    <h3 class="reviews-name">${name}</h3>
+    <p class="reviews-text-content">${rewiew}</p>
   `;
-    fragment.appendChild(rewiewer_item);
+  fragment.appendChild(rewiewer_item);
 });
 
 rewiews.appendChild(fragment);
+
+const rev_btn_next = document.querySelector('.reviews-button-next');
+const rev_btn_prev = document.querySelector('.reviews-button-prev');
+const swiper = new Swiper('.review-swiper', {
+  direction: 'horizontal',
+  // loop: true,
+  simulateTouch: true,
+  spaceBetween: 16,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 16,
+    },
+  },
+  modules: [Navigation],
+    navigation: {
+        nextEl: rev_btn_next,
+        prevEl: rev_btn_prev,  
+  },
+  on: {
+    reachEnd: () => {
+      rev_btn_next.disabled = true;
+    },
+    reachBeginning: () => {
+      rev_btn_prev.disabled = true;
+    },
+    fromEdge: () => {
+      rev_btn_next.disabled = false;
+      rev_btn_prev.disabled = false;
+    }
+    }
+});
