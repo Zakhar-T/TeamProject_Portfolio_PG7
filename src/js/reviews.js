adb7d070bbfdcf807dc704ac1ece12974b28f8bd
GordonVL <ladaniv123@gmail.com>
2025-04-05 23:33:41 +0200

    fix link

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -6,28 +6,28 @@ const rewiewers_list = [
   {
     name: 'Natalie',
     alt: 'natalia',
-    img: '../img/rewiews/natalia.png',
+    img: './img/rewiews/natalia.png',
     rewiew:
       'Work with was extraordinary! He turned out to be a very competent and responsible specialist. The projects were completed on time and the result exceeded my expectations',
   },
   {
     name: 'Dmytro',
     alt: 'dmytro',
-    img: '../img/rewiews/dmytro.png',
+    img: './img/rewiews/dmytro.png',
     rewiew:
       'I have the honor to recommend him as an exceptional professional in his field. His knowledge and expertise are undeniable. Cooperation with him always brings impressive results.',
   },
   {
     name: 'Anna',
     alt: 'anna',
-    img: '../img/rewiews/anna.png',
+    img: './img/rewiews/anna.png',
     rewiew:
       'The developed project impresses with its quality and efficiency. The code is cleanly written and the functionality exceeds expectations. Extremely satisfied with the cooperation!',
   },
   {
     name: 'Ivetta',
     alt: 'ivetta',
-    img: '../img/rewiews/ivetta.png',
+    img: './img/rewiews/ivetta.png',
     rewiew:
       'Thanks for the excellent work on the project! His talent and professionalism deserve recognition. I recommend it to everyone who is looking for an expert in the field of software development.',
   },
