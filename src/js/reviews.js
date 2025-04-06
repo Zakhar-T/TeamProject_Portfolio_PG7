478d3375796266c430a1065293cdb2e579b258f4
GordonVL <ladaniv123@gmail.com>
2025-04-06 14:07:42 +0200

    fixed section reviews

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,49 +1,30 @@
 import Swiper from 'swiper';
 import 'swiper/css';
-import { Navigation} from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
+import axios from 'axios';
 
-const rewiewers_list = [
-  {
-    name: 'Natalie',
-    alt: 'natalia',
-    img: './img/rewiews/natalia.png',
-    rewiew:
-      'Work with was extraordinary! He turned out to be a very competent and responsible specialist. The projects were completed on time and the result exceeded my expectations',
-  },
-  {
-    name: 'Dmytro',
-    alt: 'dmytro',
-    img: './img/rewiews/dmytro.png',
-    rewiew:
-      'I have the honor to recommend him as an exceptional professional in his field. His knowledge and expertise are undeniable. Cooperation with him always brings impressive results.',
-  },
-  {
-    name: 'Anna',
-    alt: 'anna',
-    img: './img/rewiews/anna.png',
-    rewiew:
-      'The developed project impresses with its quality and efficiency. The code is cleanly written and the functionality exceeds expectations. Extremely satisfied with the cooperation!',
-  },
-  {
-    name: 'Ivetta',
-    alt: 'ivetta',
-    img: './img/rewiews/ivetta.png',
-    rewiew:
-      'Thanks for the excellent work on the project! His talent and professionalism deserve recognition. I recommend it to everyone who is looking for an expert in the field of software development.',
-  },
-];
+async function getData() {
+  try {
+    const response = await axios.get('https://portfolio-js.b.goit.study/api/reviews');
+    return response.data;
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
 
+const rewiewers_list = await getData();
 const rewiews = document.querySelector('.reviews-list');
 const fragment = document.createDocumentFragment();
 
-rewiewers_list.forEach(({ name, alt, img, rewiew }) => {
+rewiewers_list.forEach(({ author, avatar_url, review }) => {
   const rewiewer_item = document.createElement('li');
   rewiewer_item.classList.add('reviews-item');
   rewiewer_item.classList.add('swiper-slide');
   rewiewer_item.innerHTML = `
-    <img src="${img}" alt="${alt}" class="reviews-img">
-    <h3 class="reviews-name">${name}</h3>
-    <p class="reviews-text-content">${rewiew}</p>
+    <img src="${avatar_url}" alt="${author} review" class="reviews-img">
+    <h3 class="reviews-name">${author}</h3>
+    <p class="reviews-text-content">${review}</p>
   `;
   fragment.appendChild(rewiewer_item);
 });
@@ -52,9 +33,8 @@ rewiews.appendChild(fragment);
 
 const rev_btn_next = document.querySelector('.reviews-button-next');
 const rev_btn_prev = document.querySelector('.reviews-button-prev');
-const swiper = new Swiper('.review-swiper', {
+const swiper = new Swiper('.reviews-swiper', {
   direction: 'horizontal',
-  // loop: true,
   simulateTouch: true,
   spaceBetween: 16,
   breakpoints: {
@@ -72,9 +52,9 @@ const swiper = new Swiper('.review-swiper', {
     },
   },
   modules: [Navigation],
-    navigation: {
-        nextEl: rev_btn_next,
-        prevEl: rev_btn_prev,  
+  navigation: {
+    nextEl: rev_btn_next,
+    prevEl: rev_btn_prev,
   },
   on: {
     reachEnd: () => {
@@ -86,6 +66,34 @@ const swiper = new Swiper('.review-swiper', {
     fromEdge: () => {
       rev_btn_next.disabled = false;
       rev_btn_prev.disabled = false;
-    }
-    }
+    },
+  },
+});
+
+
+const reviews_card = document.querySelectorAll('.reviews-item');
+
+reviews_card.forEach(card => {
+  card.addEventListener('mouseenter', event => {
+    const rect = card.getBoundingClientRect();
+    const cardClone = card.cloneNode(true);
+    card.style.opacity = 0;
+    cardClone.classList.add('card-clone');
+    cardClone.classList.add('card-open');
+    cardClone.style.position = 'absolute';
+    cardClone.style.top = `${rect.top + window.scrollY}px`; 
+    cardClone.style.left = `${rect.left + window.scrollX}px`;
+    cardClone.style.width = `${rect.width}px`;
+    cardClone.style.maxHeight = 'max-content';
+    cardClone.style.zIndex = '1000';
+    cardClone.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.2)'; 
+    cardClone.style.backgroundColor = '#1c1d22';
+
+    document.body.appendChild(cardClone);
+
+    cardClone.addEventListener('mouseleave', () => {
+      cardClone.remove();
+      card.style.opacity = 1;
+    });
+  });
 });
